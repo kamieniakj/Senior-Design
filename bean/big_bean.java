734d75a03786f27a3175e8892785466b45458fd9
Jacob K <none>
2012-10-25 13:50:31 -0400

    added the bean

---
+++ bean/big_bean.java
@@ -0,0 +1,69 @@
+package bean;
+
+public class big_bean{
+
+    String username;
+    String password;
+    String ssn;
+    String account;
+    String add;
+    String newpass;
+    String rem;
+
+    public void setUsername(String value){
+
+        username = value;
+
+    }
+
+    public void setPassword(String value){
+
+        password = value;
+    
+    }
+
+    public void setSsn(String value){
+
+        ssn = value;
+
+    }
+    
+    public void setAccount(String value){
+
+        account = value;
+
+    }
+    
+    public void setAdd(String value){
+
+        add = value;
+
+    }
+    
+    public void setNewpass(String value){
+
+        newpass = value;    
+
+    }
+
+    public void setRem(String value){
+
+        rem = value;
+
+    }
+
+    public String getUsername(){ return username; }
+
+    public String getPassword(){ return password; }
+
+    public String getSsn(){ return ssn; }
+    
+    public String getAccount(){ return account; }
+    
+    public String getAdd(){ return add; }
+    
+    public String getNewpass(){ return newpass; }
+
+    public String getRem(){ return rem; }
+
+}//END
