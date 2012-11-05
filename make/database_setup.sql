308cb8c33d8e45f672dd2aca4b3f247807b787cc
Jacob K <none>
2012-11-05 16:49:06 -0500

    added a sql script that populates the test database, as well as instructions

---
+++ make/database_setup.sql
@@ -0,0 +1,6 @@
+CREATE TABLE clients ( username CHAR(20), password CHAR(20), admin CHAR(20), ssn CHAR(20) );
+INSERT INTO clients (username, password, admin, ssn) VALUES("admin", "pass", "1", "");
+INSERT INTO clients (username, password, admin, ssn) VALUES("jacob", "pass", "0", "111");
+INSERT INTO clients (username, password, admin, ssn) VALUES("naman", "pass", "0", "222");
+INSERT INTO clients (username, password, admin, ssn) VALUES("brandon", "pass", "0", "333");
+INSERT INTO clients (username, password, admin, ssn) VALUES("mourya", "pass", "0", "444");
