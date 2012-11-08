b73c1bf516d6611e5d82a80c87334b84097a13e7
Jacob K <none>
2012-11-07 19:30:38 -0500

    increased char length for variables

--- make/database_setup.sql
+++ make/database_setup.sql
@@ -1,4 +1,4 @@
-CREATE TABLE clients ( username CHAR(20), password CHAR(20), admin CHAR(20), ssn CHAR(20) );
+CREATE TABLE clients ( username CHAR(255), password CHAR(255), admin CHAR(255), ssn CHAR(255) );
 INSERT INTO clients (username, password, admin, ssn) VALUES("admin", "pass", "1", "");
 INSERT INTO clients (username, password, admin, ssn) VALUES("jacob", "pass", "0", "111");
 INSERT INTO clients (username, password, admin, ssn) VALUES("naman", "pass", "0", "222");
